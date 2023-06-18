c9cec20a3ef6e6901022c871daa5c4836ae83174
Artur Peiev <fromtheisland13@gmail.com>
2023-06-18 15:45:06 +0100

    wigs_landing

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const humburger = document.querySelector('.humburger');
+const sideNav = document.querySelector('.sidenav');
+const imagesBlock = document.querySelector('.images');
+const imagesBox = document.querySelector('.images__box');
+const preSale = document.querySelector('.images__wrapper_presale');
+const sideClose = document.querySelector('.sidenav__close');
+
+
+humburger.addEventListener('click', () => {
+    
+    document.createElement('div');
+    /* imagesBlock.classList.add('active__class'); */
+    /* preSale.style.display = 'none'; */
+    preSale.classList.add('active__class');
+    preSale.classList.remove('images__wrapper_presale')
+    sideNav.style.transform = 'translateX(0%)';
+    sideNav.style.transition = '0.5s';
+   
+});
+sideClose.addEventListener('click', ()=> {
+    imagesBlock.classList.remove('active__class');
+    preSale.classList.add('images__wrapper_presale')
+    /* preSale.style.display = 'block'; */
+    sideNav.style.transform = 'translateX(-100%)';
+    imagesBox.style.display = 'block';
+
+})
+
+
+
